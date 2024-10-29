cf63de5496ac808ae065e64627385408a19c4992
Yaroslav <garasym@gmail.com>
2024-10-29 19:49:28 +0200

    project template

---
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -0,0 +1,19 @@
+package com.softserve.edu;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void checkApp() {
+        //int i = 0;
+        //i = 10 / i;
+        Assertions.assertEquals(4, 2 + 2);
+        System.out.println("done");
+    }
+
+}
